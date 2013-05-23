cf4d7d6903a71410a9d6d00487107f8c399c7416
Jason Grant <mrjaygee@gmail.com>
2013-05-23 10:17:02 -0600

    added a bunch of new stuff... fonts specifically

--- setup.php
+++ setup.php
@@ -14,34 +14,90 @@
 <body>
 <header id="navbar">
 	<div class="row collapse">
-		<div id="main-nav" class="large-16 columns">
+		<div class="large-16 columns">
 			<div id="logo" class="large-2 columns">
 				<img src="img/via.png">
 			</div>
-			<div class="large-10 columns">
-				<nav>
+			<div class="large-8 columns">
+				<nav id="main-nav">
 					<ul>
 						<li class="active"><a>Home</a></li>
 						<li><a>Reporting History</a></li>
 						<li><a>Support</a></li>
 					</ul>
 				</nav>
-			</div>
+			</div>	
+			<div id="utilities" class="large-2 column">
+				<p>Jiun-Kuei Jung <i class="social-foundicon-torso"></i> &#9660;</p>
+			</div>		
 		</div>
 	</div>
 	<div id="sub-nav">
 		<div class="row collapse">
-			<div class="large-16 columns">
+			<div class="large-12 columns">
 				<nav>
 					<ul>
-						<li class="active"><a>AAC</a></li>
-						<li><a>MPEG-4</a></li>
+						<li class="active"><a>AAC</a><div class="arrow-down"></div></li>
+						<li><a>MPEG-4</a><div class="arrow-down"></div></li>
 					</ul>
 				</nav>
 			</div>
 		</div>
 	</div>
 </header>
+<div id="content" class="row collapse">
+	<h2>Reporting for AAC License</h2>
+		<section class="large-12 columns">
+			<div class="large-2 columns">
+				<h3>Notices</h3>
+			</div>
+			<div class="large-9 large-offset-1 columns">
+				<div class="large-1 column"><i class="general-foundicon-website large"></i></div>
+				<div class="large-11 columns"><p>There is a new Patent List for AAC that may affect your Q3 2011 report.<br>Please <a href="#">download</a> and review.</p></div>
+			</div>
+		</section>
+		
+		<section class="large-12 columns">
+			<div class="large-2 columns">
+				<h3>In Progress</h3>
+				<p class="helper-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non.
+				</p>
+			</div>
+			<div class="large-9 large-offset-1 columns">
+				<article class="placard">
+					<h5 class="subheader">AAC</h5>					<h2>Q4<br>2012</h2>
+					<small><strong>Last Edited:</strong> 05/21/2013</small>
+					<hr>
+					<button class="tiny secondary">Continue Report</button>
+				</article>				
+			</div>
+		</section>
+		
+		<section class="large-12 columns">
+			<div class="large-2 columns">
+				<h3>Need to File</h3>
+				<p class="helper-text">You must finish the current, in progress reports before you can report on these open quarters. <a href="#">Why is this?</a></p>
+			</div>
+			<div class="large-9 large-offset-1 columns">
+				<article class="placard">
+					<h5 class="subheader">AAC</h5>					<h2>Q4<br>2012</h2>
+					<small><strong>Last Edited:</strong> 05/21/2013</small>
+				</article>
+				<article class="placard">
+					<h5 class="subheader">AAC</h5>					<h2>Q4<br>2012</h2>
+					<small><strong>Last Edited:</strong> 05/21/2013</small>
+				</article>	
+				<article class="placard">
+					<h5 class="subheader">AAC</h5>					<h2>Q4<br>2012</h2>
+					<small><strong>Last Edited:</strong> 05/21/2013</small>
+				</article>
+				<article class="placard">
+					<h5 class="subheader">AAC</h5>					<h2>Q4<br>2012</h2>
+					<small><strong>Last Edited:</strong> 05/21/2013</small>
+				</article>			
+			</div>
+		</section>
+</div>
   <script>
   document.write('<script src=' +
   ('__proto__' in {} ? 'javascripts/vendor/zepto' : 'javascripts/vendor/jquery') +
